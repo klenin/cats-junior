d3f08a3777258479661cf90ee89098d3de50acf9
Lena <elvslv@gmail.com>
2012-03-11 20:11:05 +1100

    add computing expressions in watchlist

--- interface.js
+++ interface.js
@@ -353,29 +353,64 @@ function testChanged()
 	codeareas[getCurProblem()].setValue(tests[$('#selectTest :selected').val()]);
 }
 
-function calculateValue(name)
+function calculateExpression(expression)
 {
-	var result = undefined;
-	var problem = getCurProblem();
-	if ($scope[problem] != undefined && $loc[problem] != undefined)
+	if (expression._astname == 'Name')
 	{
-		var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
-		var t_scope = $scope[problem], 
-			t_scopename = $scopename[problem], 
-			t_scopestack = $scopestack[problem];
-		while(eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) == undefined
-			&& eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentStack") != undefined)
+		var result = undefined;
+		var problem = getCurProblem();
+		if ($scope[problem] != undefined && $loc[problem] != undefined)
 		{
-			t_scope = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parent");
-			var nm = t_scopename;
-			t_scopename = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentName");
-			t_scopestack = eval("$loc[" + problem + "]." + nm + ".stack[" + t_scopestack + "].parentStack");
+			var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
+			var t_scope = $scope[problem], 
+				t_scopename = $scopename[problem], 
+				t_scopestack = $scopestack[problem];
+			var name = expression.id.v;
+			//find name 
+			while(eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) == undefined
+				&& eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentStack") != undefined)
+			{
+				t_scope = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parent");
+				var nm = t_scopename;
+				t_scopename = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentName");
+				t_scopestack = eval("$loc[" + problem + "]." + nm + ".stack[" + t_scopestack + "].parentStack");
+			}
+			result = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) !== undefined ?
+						eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name):
+						Sk.misceval.loadname(name, $gbl[problem], 1);
 		}
-		result = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) !== undefined ?
-					eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name):
-					Sk.misceval.loadname(name, $gbl[problem], 1);
+		return result;
 	}
-	return result;
+	if (expression._astname == 'Num')
+		return expression.n;
+	if (expression._astname == 'BinOp')
+	{
+		var a = calculateExpression(expression.left);
+		var b = calculateExpression(expression.right);
+		return Sk.abstr.boNumPromote_[expression.op.prototype._astname](a, b);
+	}
+	if (expression._astname == 'UnaryOp')
+	{
+		var v = calculateExpression(expression.operand);
+		var op = expression.op.prototype._astname;
+	    if (op === "USub") return -v;
+        if (op === "UAdd") return v;
+        if (op === "Invert") return ~v;
+	}
+}
+
+function calculateValue(source)
+{
+	var filename = '<stdin>.py'
+	var cst = Sk.parse(filename, source);
+    var ast = Sk.astFromParse(cst, filename);
+	var st = Sk.symboltable(ast, filename);
+	if (!(ast.body && ast.body.length == 1))
+		return 'Invalid expression';
+    var expr = ast.body[0];
+	if (expr._astname != 'Expr')
+		return 'Invalid expression';
+	return calculateExpression(expr.value);
 }
 
 function my_function()
