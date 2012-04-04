46b56e6a7fd744803e48883f54b0c82dc1d94bbe
evslv <elvslv@gmail.com>
2012-04-05 01:17:55 +1100

    add execution demonstration in code mode

--- functions.js
+++ functions.js
@@ -592,82 +592,6 @@ var Block = $.inherit({
 		return str;
 	},
 	generateCommand: function(tree, node){
-		/*str = str ?  str : 'block';
-		var newContainer = '#sortable' + str + cmdId;
-		if (container != '#sortable' + curProblem.tabIndex){
-			$(container).append('<ul id = "sortable' + str + cmdId + '" class = "ui-sortable sortable connectedSortable" style = "height: 200px; width: 220px;">');
-
-			$('#sortable' + str + cmdId).sortable({
-				revert: false,
-				cursor: 'move',
-				appendTo: 'body',
-				helper: 'clone',
-			}).disableSelection();
-			$('#sortable' + str + cmdId).prop('sortName', 'sortable' + str + cmdId);
-			$('#sortable' + str + cmdId).prop('cmdId', cmdId);
-			$('#sortable' + str + cmdId).bind('sortbeforestop', function(event, ui) {
-				cmdAdded = true;
-				var item = ui.helper.is(':visible') ? ui.helper : ui.item;
-				if (item.offset().left > $(this).offset().left + parseInt($(this).css('width')) / 2 ||
-					item.offset().left + parseInt(item.css('width'))/2 < $(this).offset().left ||
-					item.offset().top > $(this).offset().top + parseInt($(this).css('height')) / 2 ||
-					item.offset().top + 10 < $(this).offset().top)
-				{
-					ui.item.remove();
-					updated();
-					return;
-				}
-				var id = "";
-				for (var k = 0; k < classes.length; ++k)
-				{
-					if (ui.item.hasClass(classes[k]))
-					{
-						id = classes[k];
-						break;
-					}
-				}
-				id += cmdId;
-				if (!ui.item.prop('numId')){
-					ui.item.prop('id', id);
-					ui.item.prop('ifLi', 1);
-					ui.item.prop('numId', cmdId);
-					for (var j = 0; j < classes.length; ++j)
-						if (ui.helper.hasClass(classes[j])){
-							addNewCmd(classes[j], false, ui.item[0]);
-						}
-				}
-				$('#cons0').append('sortbeforestop #sortable' + str + $(this).prop('cmdId') + '\n');
-				curProblem.cmdListEnded = false;
-			});
-			$('#sortable' + str + cmdId).bind('sortstop', function(event, ui) {
-				++stoppedLvl;
-				$('#cons0').append('sortstop #sortable' + str +  $(this).prop('cmdId') + '\n');
-
-			});
-			$('#sortable' + str + cmdId).bind('sortreceive', function(event, ui) {
-				$('#cons0').append('sortreceive #sortable' + str +  $(this).prop('cmdId') + '\n');
-
-			});
-			$('#sortable' + str + cmdId).bind('sortout', function(event, ui) {
-				var i = 0;	
-			});
-			$('#sortable' + str + cmdId).bind('click', function(event, ui) {
-				if (!curProblem.playing)
-					showCounters();
-			});
-			var sortables =  $('#' + str + curProblem.tabIndex).draggable('option', 'connectToSortable');
-			sortables = '#sortable' + str + cmdId + ', ' + sortables;
-			for (var k = 0; k < classes.length; ++k){
-				$('#' + classes[k] + curProblem.tabIndex).draggable('option', 'connectToSortable', sortables);
-			}
-		}
-		else
-			newContainer = container;
-		$(newContainer).prop('type', str);
-		for (var i = 0; i < this.commands.length; ++i){
-			++cmdId;
-			this.commands[i].generateCommand(newContainer);
-		}*/
 		for (var i = 0; i < this.commands.length; ++i)
 		{
 			this.commands[i].generateCommand(tree, node ? node : 0);
@@ -675,6 +599,221 @@ var Block = $.inherit({
 	}
 });
 
+
+var curCodeMirror;
+
+function outf(text)
+{
+	text = text.replace(/</g, '&lt;');
+	$('#cons' + getCurProblem()).append(text);
+}
+
+function getCurBlock()
+{
+	var problem = getCurProblem();
+	var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
+	return eval('$loc[' + problem + '].' + scope + '.stack[$loc[' + problem + '].' + scope + '.stack.length - 1].blk');
+}
+
+function getScope()
+{
+	var problem = getCurProblem();
+	return finalcode[problem].compiled.scopes[$scope[problem]];
+}
+
+function tryNextStep(dontHiglight)
+{
+	var problem = getCurProblem();
+	if (getCurBlock() >= 0)
+	{
+		if (nextline[problem] != undefined && !dontHiglight)
+			codeareas[problem].setLineClass(nextline[problem], null);
+		var e = 1;
+		while (getCurBlock() >= 0 && (e || $expr[problem]))
+		{
+			$expr[problem] = 0;
+			e = getScope().blocks[getCurBlock()].expr;
+			try
+			{
+				eval(finalcode[problem].code);
+				updateWatchList();
+			}catch(e)
+			{
+				$('#cons' + problem).append('\n' + e + '\n');
+				return 0;
+
+			}
+		}
+		drawLabirint();
+		if (getCurBlock() >= 0)
+		{
+			var b = getCurBlock();
+			while(getScope().blocks[b].funcdef)
+				++b;
+			nextline[problem] = getScope().blocks[b].lineno;		
+		}
+			
+		if (nextline[problem] != undefined)
+		{
+			if(!dontHiglight)
+				codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
+			if (codeareas[problem].lineInfo(nextline[problem]).markerText)
+			{
+				curProblem.paused = true;
+				//curProblem.playing = false;
+				return 1;
+			}
+		}
+		if (getCurBlock() < 0)
+		{
+			if (nextline[problem] != undefined && !dontHiglight)
+				codeareas[problem].setLineClass(nextline[problem], null);
+			$('#cons' + problem).append('\nfinished\n');
+			//curProblem.stopped = true;
+			curProblem.playing = false;
+			return 0;
+		} 
+	}
+	else
+	{
+		if (nextline[problem] != undefined)
+			codeareas[problem].setLineClass(nextline[problem], null);
+		$('#cons' + problem).append('\nfinished\n');
+		//curProblem.stopped = true;
+		curProblem.playing = false;
+		return 0;
+	}
+	return 1;
+}
+
+function getCurProblem()
+{
+	return $('#tabs').tabs('option', 'selected') - 1;
+}
+
+function updateWatchList()
+{
+	var problem = getCurProblem();
+	for(var p in watchList[problem])
+	{
+		var res = calculateValue(watchList[problem][p]);
+		$('#calcVal_' + problem + '_' + p).html(res == undefined ? 'undefined' : res);
+	}
+}
+
+function tryCode()
+{
+	var problem = problems.length + 1;
+	var output = $('#cons' + problem);
+	output.html('');
+	Sk.configure({output:outf, 'problem': problem});
+	var input = codeareas[problem].getValue();
+	try {
+		finalcode[problem] = Sk.importMainWithBody("<stdin>", false, input);
+		$scope[problem] = 0,
+		$gbl[problem] = {},
+		$loc[problem] = $gbl[problem];
+		for (var i = 0; i < finalcode[problem].compiled.scopes.length; ++i)
+		{
+			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + ' = {};');
+			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + '.defaults = [];');
+			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + '.stack = [];');
+		}
+		eval('$loc[' + problem + '].scope0.stack.push({"loc": {}, "param": {}, blk: 0});');
+		nextline[problem] = getScope().firstlineno;
+		codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
+		if (codeareas[problem].lineInfo(nextline[problem]).markerText)
+		{
+			curProblem.paused = true;
+			curProblem.playing = false;
+		}
+		$scopename[problem] = finalcode[problem].compiled.scopes[0].scopename;
+		$scopestack[problem] = 0;
+		$('#codeRes1').html(finalcode[problem].code);
+		$gbl[problem]['my_function'] = my_function;
+		updateWatchList();
+	} catch (e) {
+		alert(e);
+	}
+}
+
+function showHideCode()
+{
+	if ($('#showHide').prop('checked'))
+		$('#codeRes1').hide();
+	else
+		$('#codeRes1').show();
+}
+
+function testChanged()
+{
+	codeareas[getCurProblem()].setValue(tests[$('#selectTest :selected').val()]);
+}
+
+function calculateExpression(expression)
+{
+	if (expression._astname == 'Name')
+	{
+		var result = undefined;
+		var problem = getCurProblem();
+		if ($scope[problem] != undefined && $loc[problem] != undefined)
+		{
+			var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
+			var t_scope = $scope[problem], 
+				t_scopename = $scopename[problem], 
+				t_scopestack = $scopestack[problem];
+			var name = expression.id.v;
+			//find name 
+			while(eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) == undefined
+				&& eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentStack") != undefined)
+			{
+				t_scope = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parent");
+				var nm = t_scopename;
+				t_scopename = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentName");
+				t_scopestack = eval("$loc[" + problem + "]." + nm + ".stack[" + t_scopestack + "].parentStack");
+			}
+			result = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) !== undefined ?
+						eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name):
+						Sk.misceval.loadname(name, $gbl[problem], 1);
+		}
+		return result;
+	}
+	if (expression._astname == 'Num')
+		return expression.n;
+	if (expression._astname == 'BinOp')
+	{
+		var a = calculateExpression(expression.left);
+		var b = calculateExpression(expression.right);
+		return Sk.abstr.boNumPromote_[expression.op.prototype._astname](a, b);
+	}
+	if (expression._astname == 'UnaryOp')
+	{
+		var v = calculateExpression(expression.operand);
+		var op = expression.op.prototype._astname;
+	    if (op === "USub") return -v;
+        if (op === "UAdd") return v;
+        if (op === "Invert") return ~v;
+	}
+}
+
+function calculateValue(source)
+{
+	var filename = '<stdin>.py'
+	var cst = Sk.parse(filename, source);
+    var ast = Sk.astFromParse(cst, filename);
+	var st = Sk.symboltable(ast, filename);
+	if (!(ast.body && ast.body.length == 1))
+		return 'Invalid expression';
+    var expr = ast.body[0];
+	if (expr._astname != 'Expr')
+		return 'Invalid expression';
+	return calculateExpression(expr.value);
+}
+
+function my_function()
+{
+	alert('yeah!!!');
+}
 
 function callScript(url, callback){
 	if (atHome){
@@ -991,6 +1130,7 @@ function drawLabirint(){
 }
 
 function setDefault(f){
+	var problem = curProblem.tabIndex;
 	for (var i = 0; i < btns.length; ++i)
 		$('#btn_' + btns[i] + curProblem.tabIndex).button('enable');	
 	$('#sortable' + curProblem.tabIndex).sortable('enable');
@@ -1039,6 +1179,14 @@ function setDefault(f){
 	//$("#cons" + curProblem.tabIndex).empty();
 	curProblem.cmdList.setDefault();
 	enableButtons();
+	finalcode[problem] = undefined;
+	$scope[problem] = undefined,
+	$gbl[problem] = undefined,
+	$loc[problem] = $gbl[problem];
+	nextline[problem] = undefined;
+	for (var i = 0; i < codeareas[problem].lineCount(); ++i)
+		codeareas[problem].setLineClass(i, null);
+	updateWatchList();
 }
 
 function prevDivName(){
@@ -1048,13 +1196,20 @@ function prevDivName(){
 }
 
 function loop(cnt, i){
-	curProblem.cmdList.exec(1);
-	++curProblem.step;
-	if (curProblem.cmdList.isFinished())
+	if ($('#codeMode' + curProblem.tabIndex).prop('checked'))
 	{
-		curProblem.playing = false;
-		enableButtons();
-		return;
+		tryNextStep();
+	}
+	else
+	{
+		curProblem.cmdList.exec(1);
+		++curProblem.step;
+		if (curProblem.cmdList.isFinished())
+		{
+			curProblem.playing = false;
+			enableButtons();
+			return;
+		}
 	}
 	nextStep(cnt - 1, ++i);	
 }
@@ -1122,13 +1277,15 @@ function nextStep(cnt, i){
 		enableButtons();
 		return;
 	}
-	if (cnt && !curProblem.paused)
+	if (cnt && !curProblem.paused && curProblem.playing)
 		setTimeout(function() { loop(cnt, i); }, curProblem.speed);
 	else
 	{
 		drawLabirint();
 		changeProgressBar();
 		enableButtons();
+		if (!curProblem.playing && $('#codeMode' + curProblem.tabIndex).prop('checked'))
+			onFinishExecuting(getCurProblem());
 	}
 }
 
@@ -1141,22 +1298,26 @@ function highlightLast()
 }
 
 function play(cnt){
-	if (!curProblem.playing || curProblem.arrow.dead)
-	{
-		setCounters();
-		hideCounters();
-		setDefault();
-		curProblem.playing = true;
-	}
 	if (!curProblem.speed)
 	{
-		curProblem.step += cnt - curProblem.cmdList.exec(cnt);
+		if ($('#codeMode' + curProblem.tabIndex).prop('checked'))
+		{
+			while(!curProblem.paused && tryNextStep()){};
+			if ($('#codeMode' + curProblem.tabIndex).prop('checked'))
+				onFinishExecuting(getCurProblem());
+		}
+		else
+		{
+			curProblem.step += cnt - curProblem.cmdList.exec(cnt);
+			if (curProblem.cmdList.isFinished())
+				curProblem.playing = false;
+		}
 		changeProgressBar();
 		drawLabirint();
 		enableButtons();
-		if (curProblem.cmdList.isFinished())
-			curProblem.playing = false;
+		
 		curProblem.cmdList.highlightOff();//inefficiency!!!!!!!!
+		
 		highlightLast();
 	}
 	else
--- interface.js
+++ interface.js
@@ -218,220 +218,6 @@ function changeContest(){
 	}
 }
 
-var curCodeMirror;
-
-function outf(text)
-{
-	text = text.replace(/</g, '&lt;');
-	$('#cons' + getCurProblem()).append(text);
-}
-
-function getCurBlock()
-{
-	var problem = getCurProblem();
-	var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
-	return eval('$loc[' + problem + '].' + scope + '.stack[$loc[' + problem + '].' + scope + '.stack.length - 1].blk');
-}
-
-function getScope()
-{
-	var problem = getCurProblem();
-	return finalcode[problem].compiled.scopes[$scope[problem]];
-}
-
-function tryNextStep()
-{
-	var problem = getCurProblem();
-	if (getCurBlock() >= 0)
-	{
-		if (nextline[problem] != undefined)
-			codeareas[problem].setLineClass(nextline[problem], null);
-		var e = 1;
-		while (getCurBlock() >= 0 && (e || $expr[problem]))
-		{
-			$expr[problem] = 0;
-			e = getScope().blocks[getCurBlock()].expr;
-			try
-			{
-				eval(finalcode[problem].code);
-				updateWatchList();
-			}catch(e)
-			{
-				$('#cons' + problem).append('\n' + e + '\n');
-				return 0;
-
-			}
-		}
-		drawLabirint();
-		if (getCurBlock() >= 0)
-		{
-			var b = getCurBlock();
-			while(getScope().blocks[b].funcdef)
-				++b;
-			nextline[problem] = getScope().blocks[b].lineno;		
-		}
-			
-		if (nextline[problem] != undefined)
-		{
-			codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
-			if (codeareas[problem].lineInfo(nextline[problem]).markerText)
-			{
-				curProblem.paused = true;
-				//curProblem.playing = false;
-				return 1;
-			}
-		}
-		if (getCurBlock() < 0)
-		{
-			if (nextline[problem] != undefined)
-				codeareas[problem].setLineClass(nextline[problem], null);
-			$('#cons' + problem).append('\nfinished\n');
-			curProblem.stopped = true;
-			curProblem.playing = false;
-			return 0;
-		} 
-	}
-	else
-	{
-		if (nextline[problem] != undefined)
-			codeareas[problem].setLineClass(nextline[problem], null);
-		$('#cons' + problem).append('\nfinished\n');
-		curProblem.stopped = true;
-		curProblem.playing = false;
-		return 0;
-	}
-	return 1;
-}
-
-function getCurProblem()
-{
-	return $('#tabs').tabs('option', 'selected') - 1;
-}
-
-function updateWatchList()
-{
-	var problem = getCurProblem();
-	for(var p in watchList[problem])
-	{
-		var res = calculateValue(watchList[problem][p]);
-		$('#calcVal_' + problem + '_' + p).html(res == undefined ? 'undefined' : res);
-	}
-}
-
-function tryCode()
-{
-	var problem = problems.length + 1;
-	var output = $('#cons' + problem);
-	output.html('');
-	Sk.configure({output:outf, 'problem': problem});
-	var input = codeareas[problem].getValue();
-	try {
-		finalcode[problem] = Sk.importMainWithBody("<stdin>", false, input);
-		$scope[problem] = 0,
-		$gbl[problem] = {},
-		$loc[problem] = $gbl[problem];
-		for (var i = 0; i < finalcode[problem].compiled.scopes.length; ++i)
-		{
-			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + ' = {};');
-			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + '.defaults = [];');
-			eval('$loc[' + problem + '].' + finalcode[problem].compiled.scopes[i].scopename + '.stack = [];');
-		}
-		eval('$loc[' + problem + '].scope0.stack.push({"loc": {}, "param": {}, blk: 0});');
-		nextline[problem] = getScope().firstlineno;
-		codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
-		if (codeareas[problem].lineInfo(nextline[problem]).markerText)
-		{
-			curProblem.paused = true;
-			curProblem.playing = false;
-		}
-		$scopename[problem] = finalcode[problem].compiled.scopes[0].scopename;
-		$scopestack[problem] = 0;
-		$('#codeRes1').html(finalcode[problem].code);
-		$gbl[problem]['my_function'] = my_function;
-		updateWatchList();
-	} catch (e) {
-		alert(e);
-	}
-}
-
-function showHideCode()
-{
-	if ($('#showHide').prop('checked'))
-		$('#codeRes1').hide();
-	else
-		$('#codeRes1').show();
-}
-
-function testChanged()
-{
-	codeareas[getCurProblem()].setValue(tests[$('#selectTest :selected').val()]);
-}
-
-function calculateExpression(expression)
-{
-	if (expression._astname == 'Name')
-	{
-		var result = undefined;
-		var problem = getCurProblem();
-		if ($scope[problem] != undefined && $loc[problem] != undefined)
-		{
-			var scope = finalcode[problem].compiled.scopes[$scope[problem]].scopename;
-			var t_scope = $scope[problem], 
-				t_scopename = $scopename[problem], 
-				t_scopestack = $scopestack[problem];
-			var name = expression.id.v;
-			//find name 
-			while(eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) == undefined
-				&& eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentStack") != undefined)
-			{
-				t_scope = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parent");
-				var nm = t_scopename;
-				t_scopename = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].parentName");
-				t_scopestack = eval("$loc[" + problem + "]." + nm + ".stack[" + t_scopestack + "].parentStack");
-			}
-			result = eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name) !== undefined ?
-						eval("$loc[" + problem + "]." + t_scopename + ".stack[" + t_scopestack + "].loc." + name):
-						Sk.misceval.loadname(name, $gbl[problem], 1);
-		}
-		return result;
-	}
-	if (expression._astname == 'Num')
-		return expression.n;
-	if (expression._astname == 'BinOp')
-	{
-		var a = calculateExpression(expression.left);
-		var b = calculateExpression(expression.right);
-		return Sk.abstr.boNumPromote_[expression.op.prototype._astname](a, b);
-	}
-	if (expression._astname == 'UnaryOp')
-	{
-		var v = calculateExpression(expression.operand);
-		var op = expression.op.prototype._astname;
-	    if (op === "USub") return -v;
-        if (op === "UAdd") return v;
-        if (op === "Invert") return ~v;
-	}
-}
-
-function calculateValue(source)
-{
-	var filename = '<stdin>.py'
-	var cst = Sk.parse(filename, source);
-    var ast = Sk.astFromParse(cst, filename);
-	var st = Sk.symboltable(ast, filename);
-	if (!(ast.body && ast.body.length == 1))
-		return 'Invalid expression';
-    var expr = ast.body[0];
-	if (expr._astname != 'Expr')
-		return 'Invalid expression';
-	return calculateExpression(expr.value);
-}
-
-function my_function()
-{
-	alert('yeah!!!');
-}
-
 function onAddWatchClick()
 {
 	$('#addWatchDialog').dialog('open');
@@ -951,15 +737,31 @@ function disableButtons(){
 function callPlay(s){
 	//if (!$('#sortable' + curProblem.tabIndex).sortable('toArray').length || curProblem.arrow.dead)
 	//	return;
+
+	var problem = curProblem.tabIndex;
 	if (curProblem.maxCmdNum && curProblem.divIndex == curProblem.maxCmdNum){
 		var mes = new MessageCmdLimit();
 		curProblem.arrow.dead = true;
 		return;
 	}
-	try
+	if (!curProblem.playing || curProblem.arrow.dead)
 	{
-		codeareas[curProblem.tabIndex].setValue(convertCommandsToCode());
-		prepareForExecuting(curProblem.tabIndex);
+		setCounters();
+		hideCounters();
+		setDefault();
+		//curProblem.playing = true;
+	}
+	try
+	{	
+		if (!curProblem.playing)
+		{
+			if (!$('#codeMode' + problem).prop('checked'))
+			{
+				codeareas[curProblem.tabIndex].setValue(convertCommandsToCode());
+			}
+			prepareForExecuting(problem, !curProblem.speed);
+			curProblem.playing = true;
+		}
 		curProblem.paused = false;
 		curProblem.stopped = false;
 		disableButtons();
@@ -970,24 +772,30 @@ function callPlay(s){
 	}
 	catch(e)
 	{
+		curProblem.playing = false;
 		$('#cons' + curProblem.tabIndex).html('Invalid commands');
 	}
 }
 
 function onFinishExecuting(problem)
 {
-	finalcode[problem] = undefined;
+	/*finalcode[problem] = undefined;
 	$scope[problem] = undefined,
 	$gbl[problem] = undefined,
 	$loc[problem] = $gbl[problem];
 	nextline[problem] = undefined;
 	for (var i = 0; i < codeareas[problem].lineCount(); ++i)
 		codeareas[problem].setLineClass(i, null);
-	updateWatchList();
+	updateWatchList();*/
 }
 
-function prepareForExecuting(problem)
+function prepareForExecuting(problem, dontHighlight)
 {
+	setDefault();
+	curProblem.playing = false;
+	cmdHighlightOff();
+	showCounters();
+	setCounters();
 	var output = $('#cons' + problem);
 	var input = codeareas[problem].getValue();
 	if (curProblem.maxCmdNum)
@@ -1019,46 +827,22 @@ function prepareForExecuting(problem)
 	}
 	eval('$loc[' + problem + '].scope0.stack.push({"loc": {}, "param": {}, blk: 0});');
 	nextline[problem] = getScope().firstlineno;
-	codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
+	if (!dontHighlight)
+		codeareas[problem].setLineClass(nextline[problem], 'cm-curline');
 	$scopename[problem] = finalcode[problem].compiled.scopes[0].scopename;
 	$scopestack[problem] = 0;
 	$gbl[problem]['forward'] = forward;
 	$gbl[problem]['left'] = left;
 	$gbl[problem]['right'] = right;
 	$gbl[problem]['wait'] = wait;
-	curProblem.stopped = true;
-	setDefault();
-	curProblem.playing = false;
-	cmdHighlightOff();
-	showCounters();
-	setCounters();
+	//curProblem.stopped = true;
 	updateWatchList();
 	curProblem.changed = false;
 }
 
 function playClick(){
 	var problem = getCurProblem();
-	if ($('#codeMode' + problem).prop('checked'))
-	{
-		try {
-			if (!curProblem.playing)
-			{
-				prepareForExecuting(problem);
-				curProblem.playing = true;
-			}
-			curProblem.paused = false;
-			while(!curProblem.paused && tryNextStep()){};
-			if (!curProblem.playing)
-				onFinishExecuting(getCurProblem());
-		} catch (e) {
-			curProblem.playing = false;
-			alert(e);
-		}
-	}
-	else
-	{
-		callPlay(300);
-	}
+	callPlay(300);
 	$('#btn_play'+ curProblem.tabIndex).addClass('ui-state-focus');
 }
 
@@ -1079,14 +863,14 @@ function stopClick(){
 	if ($('#codeMode' + problem).prop('checked'))
 		onFinishExecuting(problem);
 	curProblem.stopped = true;
-	if (!curProblem.playing || !curProblem.speed)
-	{
+	//if (!curProblem.playing || !curProblem.speed)
+	//{
 		setDefault();
 		cmdHighlightOff();
 		showCounters();
 		setCounters();
 		curProblem.playing = false;
-	}
+	//}
 }
 
 function pauseClick(){
