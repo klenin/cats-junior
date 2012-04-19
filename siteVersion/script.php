cb5d3ff1a4493ad06412c33dd34724e2293c50f9
evslv <elvslv@gmail.com>
2012-04-20 02:02:59 +1100

    fix presentation and report

--- siteVersion/script.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//$url = 'http://imcs.dvgu.ru/cats/main.pl?f=login;login=test;passwd=test;json=1;';
-	foreach($_POST as $key=>$value ) ${$key}=$value;
-	foreach($_GET as $key=>$value ) ${$key}=$value;
-	$json = file_get_contents($url);
-	echo $json;
-?>
