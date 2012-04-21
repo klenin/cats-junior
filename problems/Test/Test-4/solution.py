21568150e18408dbe4d55e018974c39c8b65f7e3
evslv <elvslv@gmail.com>
2012-04-22 00:48:29 +1100

    add 2 problems

---
+++ problems/Test/Test-4/solution.py
@@ -0,0 +1,4 @@
+while not objectPosition("wall", "inFrontOf"):
+		while not objectPosition("wall", "inFrontOf"):
+				forward(1)
+		left(1)
---
+++ problems/Test/Test-5/solution.py
@@ -0,0 +1,9 @@
+while not objectPosition("wall", "inFrontOf"):
+		while not objectPosition("prize", "atTheRight"):
+				forward(1)
+		right(1)
+		forward(1)
+		left(2)
+		forward(1)
+		right(1)
+		forward(1)
