e165365ab37648d32a63eb1a378b15bf3b32c958
evslv <elvslv@gmail.com>
2012-04-26 22:27:33 +1100

    fix checker

--- problems/Test/Test-6/Main.py
+++ problems/Test/Test-6/Main.py
@@ -435,7 +435,7 @@ def solve():
 		curState = State(**problem)
 							
 		sol = codecs.open('output.txt', 'r', 'utf-8').read()
-		exec(sol)
+		exec(sol, {}, {'forward': forward, 'left': left, 'right': right, 'wait': wait, 'objectPosition': objectPosition})
 	except MyException as e:
 		pass
 	print(curState.pnts)
