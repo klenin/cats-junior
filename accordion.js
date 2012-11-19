59ad21f429e242ea744146af7840360b7a838df4
Lena <elvslv@gmail.com>
2012-11-19 20:37:10 +1100

    make function header editable

--- accordion.js
+++ accordion.js
@@ -1,14 +1,35 @@
 function accordionPush( id, name )
 {
-	$(id).append('<div class="jstree-draggable funccall" rel="funccall"><h3 class="func-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all"><span class="ui-accordion-header-icon ui-icon ui-icon-carat-1-n"></span><a href="#">' + name + '</a></h3><div id = "funcDef-' + name + '" style="min-height:200px" class = "func-body ui-corner-all ui-widget-content"></div></div>');
+	$(id).append(
+		'<div class="jstree-draggable funccall ui-accordion-header ui-helper-reset ui-state-default ui-corner-all" rel="funccall">' +
+			'<span class="ui-accordion-icon ui-icon ui-icon-circle-minus func-icon"></span>'+
+			'<span class="func-header">' +
+				'<label data-type="editable" data-for=".input1">' + 
+					name + 
+				'</label>' + 
+				'<input class="input1"/>' +
+			'</span>' +
+			'<div id = "funcDef-' + name + '" style="min-height:200px" class = "func-body ui-corner-all ui-widget-content"></div>' +
+		'</div>');
+	$('body').editables( 
+    { 
+    	editOn: 'dblclick',
+		beforeEdit: function(field){
+			field.val(this.text());
+		},
+		beforeFreeze: function(display){ 
+			display.text(this.val());
+		}
+	} 
+	);
 }
 
 function accordionUpdateEvents( id )
 {
-	$(id + ' >div .func-header').unbind('click').bind('click', function(eventObject){
-		$(this).next().toggle( 'fold', 1000 );
-		$(this).children('span').toggleClass( 'ui-icon-carat-1-s' );
-		$(this).children('span').toggleClass( 'ui-icon-carat-1-n' );
+	$(id + ' >div .func-icon').unbind('click').bind('click', function(eventObject){
+		$(this).next().next().toggle( 'fold', 1000 );
+		$(this).toggleClass( 'ui-icon-circle-plus' );
+		$(this).toggleClass( 'ui-icon-circle-minus' );
 		return false;
 	});
 }
