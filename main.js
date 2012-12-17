cf131af46021f6ec378ddb3c64360588061815a0
Lena <elvslv@gmail.com>
2012-12-17 22:19:26 +1100

    rename function calls on dbl click

--- main.js
+++ main.js
@@ -280,7 +280,13 @@
 		problem.updated();
 	}).bind('refresh.jstree', function(event, data) {
 		problem.updated();
-	});
+	}).bind("dblclick.jstree", function (e, data) {
+        var node = $(e.target).closest("li");
+        var type = $.jstree._reference(this)._get_type(node);
+		if (type == 'funccall') {
+			$.jstree._reference(this).rename(node);
+		}
+    });
 	$('#accordion' + problem.tabIndex).myAccordion( {'problem': problem } );
 		/*$('#accordion' + problem.tabIndex).accordion();
 		$('#accordion' + problem.tabIndex).accordion( "enable" );
