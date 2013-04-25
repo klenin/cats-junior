f8e2a216ca7b1cfcd068522567bef2f1450fbab6
Lena <elvslv@gmail.com>
2013-04-26 01:09:25 +1100

    fix problem statement

--- problems/Test/Test-11/sol.py
+++ problems/Test/Test-11/sol.py
@@ -4,32 +4,24 @@ def moveForward():
       forward(1)
       moveForward()
 def rotate():
-  if not objectPosition("wall", "atTheLeft"):
-    left(1)
-  else:
+  if objectPosition("wall", "atTheLeft"):
     right(1)
+  else:
+    left(1)
+  forward(1)
 def rotate1():
-  if not objectPosition("wall", "atTheRight"):
-    right(1)
-  else:
+  if objectPosition("wall", "atTheRight"):
     left(1)
-for i in range(13):
-  moveForward()
-  if objectPosition("wall", "inFrontOf"):
-    rotate()
-    forward(1)
   else:
-    rotate()
-    forward(2)
-    left(1)
-    forward(1)
-for i in range(17):
-  moveForward()
-  if objectPosition("wall", "inFrontOf"):
-    rotate1()
-    forward(1)
-  else:
-    rotate1()
-    forward(2)
     right(1)
-    forward(1)
+  forward(1)
+def solve(arg0):
+  for i in range(arg0):
+    moveForward()
+    rotate()
+def solve1(arg0):
+  for i in range(20):
+    moveForward()
+    rotate1()
+solve(20)
+solve1(20)
