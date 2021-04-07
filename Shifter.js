34d61cecf0029deba99c722d4701a37e1f308ecf
Sabiks-art <81517629+Sabiks-art@users.noreply.github.com>
2021-04-07 18:40:14 +1000

    shifter

--- Shifter.js
+++ Shifter.js
@@ -9,28 +9,35 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 	
 	var Pyramid = $.inherit({
 		
-		__constructor: function(rings, index, div){
+		__constructor: function(rings, index, div, num_lines, color, size){
 			this.rings = rings;
 			this.initRings = rings;
 			this.div = div;
 			this.index = index;
-			
-			this.init(); 
+			this.color = color;
+			this.size = size;
+
+			this.init(num_lines); 
 		},
 		
-		init: function() {
+		init: function(num_lines) {
+			
+			num_lines--;
+			
+			let num_ring = 0;
 			
 			let td = $('<td class="base" align="center"></td>');
 
 			for (let j = 0; j < 8; j++) td.append('<tr style = "height:21px"; width = "100%"></tr>');
 			
-			let ring_size = 30 * this.rings;
-			 for (let i = 5; i > (5 - this.rings); i--){
-				td.children("tr").eq(i).append('<div style="height:20px; width:' + String(ring_size) + 'px; background-color:red; border:red solid 0.1px; border-radius:75px"</div>');
-				ring_size -= 15;
+			for (let i = 5; i > (5 - this.rings); i--){
+				
+				td.children("tr").eq(i).append('<div style="height:20px; width:' + String(this.size[num_ring].width) + 'px; background-color:' + this.color + '; border:'+ this.color + ' solid 0.1px; border-radius:75px"</div>');
+				num_ring++;
+				
 			};   
-						
-			$('#FieldPyr').append(td);
+			
+			$('#FieldPyr' + String(num_lines)).append(td);
 			
 			
 		},
@@ -40,20 +47,25 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 			},
 			
 		draw: function(x) {
-	
-			let sup_str1 = $('#FieldPyr').find("td").eq(x).find("div").eq(0).css('width');
-			let sup_str2 = $('#FieldPyr').find("td").eq(this.index).find("div").eq(0).css('width');
+			let sup_x = x;
+			let sup_y = this.index;
+			if (x % 3 == 2) sup_x = x - 1;
+			if (this.index % 3 == 2) sup_y = this.index - 1;
+			
+			
+			let sup_str1 = $('#FieldPyr' + String(Math.floor((sup_x + 1) / 3))).find("td").eq(x - (3 * Math.floor((sup_x + 1) / 3))).find("div").eq(0).css('width');
+			let sup_str2 = $('#FieldPyr' + String(Math.floor((sup_y + 1) / 3))).find("td").eq(this.index - (3 * Math.floor((sup_y + 1) / 3))).find("div").eq(0).css('width');
 			
 			let sup_bool = false;
 			
 			if (String(sup_str1).length < String(sup_str2).length) sup_bool = true;
-			else if ((sup_str1 < sup_str2) && (String(sup_str1).length == String(sup_str2).length)) sup_bool = true;
+			else if ((sup_str1 <= sup_str2) && (String(sup_str1).length == String(sup_str2).length)) sup_bool = true;
 			
-			if (sup_bool || (!($('#FieldPyr').find("td").eq(this.index).find("div").length))){
+			if (sup_bool){
 				 
-			$('#FieldPyr').find("td").eq(this.index).find("tr").eq(6 - this.rings).append($('#FieldPyr').find("td").eq(x).find("div").eq(0));
-			
-			} else throw new IncorrectInput('Кольцо на ' + String(x + 1) + ' пирамидке больше,\nчем колько на ' + String(this.index + 1) + ' пирамидке');
+			$('#FieldPyr' + String(Math.floor((sup_y + 1) / 3))).find("td").eq(this.index - (3 * Math.floor((sup_y + 1) / 3))).find("tr").eq(6 - this.rings).append($('#FieldPyr' + String(Math.floor((sup_x + 1) / 3))).find("td").eq(x - (3 * Math.floor((sup_x + 1) / 3))).find("div").eq(0));
+
+			} else throw new IncorrectInput('Кольцо на ' + String(x + 1) + ' пирамидке больше,\nчем кольцо на ' + String(this.index + 1) + ' пирамидке');
 			
 			},
 			
@@ -81,18 +93,16 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 		if (args.length != 4 || !checkNumber(args[1]) || !checkNumber(args[3])) {
 			throw new IncorrectInput('Некорректный список аргументов');
 		}
-		
-		var first = args[1] - 1;
+
 		var comparator = args[2];
-		var second = args[3] - 1;
 		var result = false;
 
 		switch(comparator) {
 			case '<':
-				result = curProblem.executionUnit.getExecutionUnit().isLessPyramid(first, second);
+				result = curProblem.executionUnit.getExecutionUnit().isLessPyramid(args[1], args[3]);
 				break;
 			case '>':
-				result = curProblem.executionUnit.getExecutionUnit().isGreaterPyramid(first, second);
+				result = curProblem.executionUnit.getExecutionUnit().isGreaterPyramid(args[1], args[3]);
 				break;
 			}
 
@@ -108,9 +118,7 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 			throw new IncorrectInput('Некорректный аргумент');
 		}
 		
-		first -= 1;
 		comparator = comparator.v;
-		second -= 1;
 
 		switch(comparator) {
 			case '<':
@@ -145,8 +153,8 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 					this.commands = {};
 					
 					let argP = [ 
-					new ExecutionUnitCommands.CommandArgumentSpin(1, 3),
-					new ExecutionUnitCommands.CommandArgumentSpin(1, 3)
+					new ExecutionUnitCommands.CommandArgumentSpin(1, this.data.pyramids.length),
+					new ExecutionUnitCommands.CommandArgumentSpin(1, this.data.pyramids.length)
 				];
 					
 					this.commands['shift'] = new ExecutionUnitCommands.ExecutionUnitCommand('shift', shift,argP);
@@ -173,10 +181,12 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 				
 				init: function() {
 					
+					let num_lines = Math.ceil(this.data.pyramids.length / 3);
+										
 					let table = $('<table id = "TableShift" style = "width:100%; height: 170px"></table>').appendTo(this.div);
-					let tr = $('<tr id="FieldPyr" style = "height: 170px"></tr>').appendTo(table);
+					for (let i = 0; i < num_lines;i++) $('<tr id="FieldPyr' + String(i) + '" style = "height: 170px"></tr>').appendTo(table);
 					
-					for (var i = 0; i < this.data.pyramids.length; ++i)	this.pyramids.push(new Pyramid(this.data.pyramids[i].rings, i, this.div));
+					for (var i = 0; i < this.data.pyramids.length; ++i)	this.pyramids.push(new Pyramid(this.data.pyramids[i].rings.length, i, this.div, Math.ceil((i + 1)/3), this.data.pyramids[i].color, this.data.pyramids[i].rings));
 					
 					this.points = this.data.startPoints;
 				},
@@ -227,7 +237,7 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 				},
 
 				executionFinished: function(){
-					if (this.isSolved()) {
+					if (this.isSolved() == this.data.finishState.length) {
 						this.points += this.data.pointsWon;
 						var mes = new MessageWon(this.problem.step, this.points);
 					}
@@ -246,10 +256,23 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 						throw new IncorrectInput('Нет пирамидки с номером "' + args[1] + '"');
 					}
 					
+					let sup_x = x;
+					let sup_y = y;
 					
-					if ((!($('#FieldPyr').find("td").eq(this.pyramids[x].index).find("div").length))) {
+					if (x % 3 == 2) sup_x = x - 1;
+					if (y % 3 == 2) sup_y = y - 1;
+					
+					if ((!($('#FieldPyr' + String(Math.floor((sup_x + 1) / 3))).find("td").eq(x - (3 * Math.floor((sup_x + 1) / 3))).find("div").length))) {
 						throw new IncorrectInput('На пирамидке "' + args[0] + '" нет колец');
 					}
+					
+					if (($('#FieldPyr' + String(Math.floor((sup_x + 1) / 3))).find("td").eq(x - (3 * Math.floor((sup_x + 1) / 3))).find("div").length > 0) && ($('#FieldPyr' + String(Math.floor((sup_y + 1) / 3))).find("td").eq(y - (3 * Math.floor((sup_y + 1) / 3))).find("div").length > 0)) {
+						if ($('#FieldPyr' + String(Math.floor((sup_x + 1) / 3))).find("td").eq(x - (3 * Math.floor((sup_x + 1) / 3))).find("div").eq(0).css('background-color') != $('#FieldPyr' + String(Math.floor((sup_y + 1) / 3))).find("td").eq(y - (3 * Math.floor((sup_y + 1) / 3))).find("div").eq(0).css('background-color')){
+							throw new IncorrectInput('Цвета колец различны!');
+						}
+					}
+					
+					if (x == y) throw new IncorrectInput('Некорректный аргумент');
 					
 					this.pyramids[x].shiftTo();
 					this.pyramids[y].shiftFrom();
@@ -257,13 +280,21 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 				},
 				
 				isSolved: function() {
+					let result = 2;
 					for (var i = 0; i < this.data.finishState.length; ++i) {
+
 						var pyramid = this.data.finishState[i].pyramid;
-						if (this.pyramids[pyramid].rings != this.data.finishState[i].rings) {
-							return false;
+						let sup_pyr = pyramid;
+						if (pyramid % 3 == 2) sup_pyr = pyramid - 1;
+						
+						if ((this.pyramids[pyramid].rings != this.data.finishState[i].rings) || ($('#FieldPyr' + String(Math.floor((sup_pyr + 1) / 3))).find("td").eq(pyramid - (3 * Math.floor((sup_pyr + 1) / 3))).find("div").eq(0).css('background-color') != this.data.finishState[i].color)) {
+							result--;	
+
 						}
+						
 					}
-					return true;
+					console.log(String(result));
+					return result;
 				},
 				
 				draw: function() {
@@ -273,11 +304,44 @@ define('Shifter', ['jQuery', 'jQueryUI', 'jQueryInherit', 'ExecutionUnitCommands
 				},
 				
 				isLessPyramid: function(first,second) {
-					return $('#FieldPyr').find("td").eq(this.pyramids[first].index).find("div").eq(0).css('width') < $('#FieldPyr').find("td").eq(this.pyramids[second].index).find("div").eq(0).css('width')
+					
+					first--;
+					second--;
+					let sup_first = first;
+					let sup_second = second;
+					if (first % 3 == 2) sup_first = first - 1;
+					if (second % 3 == 2) sup_second = second - 1;
+					
+					let sup_str1 = $('#FieldPyr' + String(Math.floor((sup_first+1) / 3))).find("td").eq(first - (3 * Math.floor((sup_first+1) / 3))).find("div").eq(0).css('width');
+					let sup_str2 = $('#FieldPyr' + String(Math.floor((sup_second+1) / 3))).find("td").eq(second - (3 * Math.floor((sup_second+1) / 3))).find("div").eq(0).css('width');
+					let result = false;
+					
+					if ((!($('#FieldPyr' + String(Math.floor((sup_first+1) / 3))).find("td").eq(first - (3 * Math.floor((sup_first+1) / 3))).find("div").length)) || (!($('#FieldPyr' + String(Math.floor((sup_second+1) / 3))).find("td").eq(second - (3 * Math.floor((sup_second+1) / 3))).find("div").length))) return false;
+					
+					if (String(sup_str1).length < String(sup_str2).length) result = true;
+					else if ((sup_str1 < sup_str2) && (String(sup_str1).length == String(sup_str2).length)) result = true;
+
+					return  result; 
 				},
 				
 				isGreaterPyramid: function(first,second) {
-					return $('#FieldPyr').find("td").eq(this.pyramids[first].index).find("div").eq(0).css('width') > $('#FieldPyr').find("td").eq(this.pyramids[second].index).find("div").eq(0).css('width')
+					first--;
+					second--;
+					let sup_first = first;
+					let sup_second = second;
+					if (first % 3 == 2) sup_first = first - 1;
+					if (second % 3 == 2) sup_second = second - 1;
+					
+					let sup_str1 = $('#FieldPyr' + String(Math.floor((sup_first+1) / 3))).find("td").eq(first - (3 * Math.floor((sup_first+1) / 3))).find("div").eq(0).css('width');
+					let sup_str2 = $('#FieldPyr' + String(Math.floor((sup_second+1) / 3))).find("td").eq(second - (3 * Math.floor((sup_second+1) / 3))).find("div").eq(0).css('width');
+					let result = false;
+					
+					if ((!(String(sup_str1).length)) || (!(String(sup_str2).length))) return false;
+					
+					if (String(sup_str1).length > String(sup_str2).length) result = true;
+					else if ((sup_str1 > sup_str2) && (String(sup_str1).length == String(sup_str2).length)) result = true;
+
+					return  result; 
 				},
 				
 				isGameOver: function() {
